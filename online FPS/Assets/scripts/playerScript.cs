daa25344d166028229f6c4542c441c052aef7412
akuna <71008431+1akuna@users.noreply.github.com>
2021-09-26 10:03:50 +0300

    changed up some things
    changed up some things

--- online FPS/Assets/scripts/playerScript.cs
+++ online FPS/Assets/scripts/playerScript.cs
@@ -13,20 +13,11 @@ public class playerScript : NetworkBehaviour
     [SerializeField]private float mouseSens;
     [SerializeField]private float gravity;
     [SerializeField]private float jumpHeight;
-    [SerializeField]private float fireRate;
-    [SerializeField]private float bulletsInMag;
-    [SerializeField]private float reloadTime;
     [Header("Set variables: ")]
     [SerializeField]private Transform cameraTransform;
     [SerializeField]private Transform groundCheck;
     [SerializeField]private LayerMask groundMask;
-    [SerializeField]private Transform weaponHolster;
     [SerializeField]private Transform weapon;
-    [SerializeField]private Transform ADStransform;
-    [SerializeField]private Transform hipFireTransform;
-    [SerializeField]private Transform gunBarrel;
-    [SerializeField]private GameObject bulletTrail;
-    [SerializeField]private TMP_Text bulletCountText;
 
     private CharacterController controller;
     private Camera mainCam;
@@ -44,12 +35,8 @@ public class playerScript : NetworkBehaviour
     private Vector3 velocity;
 
     private bool isGrounded;
-    private bool isReloading = false;
 
     private float speed;
-
-    private float nextFire = 0f;
-    private float bulletCount = 30f;
 
 
     private void Start() 
@@ -81,71 +68,9 @@ public class playerScript : NetworkBehaviour
     private void Update() 
     {
         if(!isLocalPlayer){return;}
-        bulletCountText.text = bulletCount.ToString();
         getInputs();
         movement();
         mouseLook();
-        weaponADS();
-        if(Input.GetKey(KeyCode.R))
-        {
-            StartCoroutine(reload());
-        }
-        if(Input.GetButton("Fire1") && Time.time > nextFire && bulletCount > 0 && isReloading == false)
-        {
-            audioSource.Play();
-            nextFire = Time.time + fireRate;
-            bulletCount--;
-            CmdShoot(mainCam.transform.position, mainCam.transform.forward, gunBarrel.position, gunBarrel.rotation);
-        }
-    }
-
-    private IEnumerator reload()
-    {
-        isReloading = true;
-        yield return new WaitForSeconds(reloadTime);
-        bulletCount = 30;
-        isReloading = false;
-    }
-
-    [Command]
-    private void CmdShoot(Vector3 _mainCamPos, Vector3 _mainCamDirection, Vector3 _gunBarrelPos, Quaternion _gunBarrelRot)
-    {
-        if(Physics.Raycast(_mainCamPos, _mainCamDirection, out RaycastHit hit, Mathf.Infinity))
-        {
-            GameObject bulletTrailInstance = Instantiate(bulletTrail, _gunBarrelPos, _gunBarrelRot);
-            NetworkServer.Spawn(bulletTrailInstance);
-            spawnBulletTrail(bulletTrailInstance, _gunBarrelPos, hit.point);
-            Destroy(bulletTrailInstance, 1f);
-            if(hit.transform.tag == "remotePlayer" || hit.transform.tag == "localPlayer")
-            {
-                Debug.Log(hit.transform.name + " has been hit");
-                ClientChangeColor(hit.transform.gameObject);
-            }
-            if(hit.transform.tag == "target")
-            {
-                ClientChangeColor(hit.transform.gameObject);
-            }
-        }
-    }
-    [ClientRpc]
-    private void spawnBulletTrail(GameObject bulletTrailInstance, Vector3 _gunBarrelPos, Vector3 hitPoint)
-    {
-        LineRenderer lineR = bulletTrailInstance.GetComponent<LineRenderer>();
-        lineR.SetPosition(0, _gunBarrelPos);
-        lineR.SetPosition(1, hitPoint);
-    }
-
-    private void weaponADS()
-    {
-        if(Input.GetKey(KeyCode.Mouse1))
-        {
-            weapon.transform.position = ADStransform.position;
-        }
-        else if(!Input.GetKey(KeyCode.Mouse1))
-        {
-            weapon.transform.position = hipFireTransform.position;
-        }
-
     }
 
     private void mouseLook()
@@ -188,24 +113,10 @@ public class playerScript : NetworkBehaviour
             //isSprinting
             speed = sprintSpeed;
         }
-        else
+        else if(!Input.GetKey(KeyCode.LeftShift))
         {
             //notSprinting
             speed = normalSpeed;
-        }
-    }
-    [ClientRpc]
-    private void ClientChangeColor(GameObject target)
-    {
-        if(target.tag == "target")
-        {
-            target.GetComponent<MeshRenderer>().material.color = Color.red;
-        }
-        else
-        {
-            MeshRenderer targetMeshRenderer = target.transform.Find("playerModel").GetComponent<MeshRenderer>();
-            Color playerStartColor = targetMeshRenderer.material.color;
-            targetMeshRenderer.material.color = Color.red;
         }
     }
 }
---
+++ online FPS/Assets/weapons.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using TMPro;
+using UnityEngine;
+
+public class weapons : NetworkBehaviour
+{
+    [SerializeField]private GameObject bulletTrail;
+    [SerializeField]private GameObject bulletHolePrefab;
+    [SerializeField]private Transform weaponHolster;
+    [SerializeField]private Transform ADStransform;
+    [SerializeField]private Transform hipFireTransform;
+    [SerializeField]private TMP_Text bulletCountText;
+    [SerializeField]private LayerMask canBeShot;
+
+    [Header("Gun Cusomization: ")]
+    [SerializeField]private Transform weapon;
+    [SerializeField]private float fireRate;
+    [SerializeField]private float bulletsInMag;
+    [SerializeField]private float reloadTime;
+    [SerializeField]private Transform gunBarrel;
+
+    private AudioSource audioSource;
+    private Camera mainCam;
+
+    private bool isReloading;
+    private float nextFire = 0f;
+    private float bulletCount = 30f;
+
+    private void Start() 
+    {
+        if(!isLocalPlayer){return;}
+        audioSource = GameObject.Find("SFX").GetComponent<AudioSource>();
+        mainCam = Camera.main;
+    }
+
+    private void Update() 
+    {
+        if(!isLocalPlayer){return;}
+        bulletCountText.text = bulletCount.ToString();
+        weaponADS();
+        if(Input.GetKey(KeyCode.R))
+        {
+            StartCoroutine(reload());
+        }
+        if(Input.GetButton("Fire1") && Time.time > nextFire && bulletCount > 0 && isReloading == false)
+        {
+            audioSource.Play();
+            nextFire = Time.time + fireRate;
+            bulletCount--;
+            CmdShoot(mainCam.transform.position, mainCam.transform.forward, gunBarrel.position, gunBarrel.rotation);
+        }
+    }
+
+    private IEnumerator reload()
+    {
+        isReloading = true;
+        yield return new WaitForSeconds(reloadTime);
+        bulletCount = 30;
+        isReloading = false;
+    }
+
+    [Command]
+    private void CmdShoot(Vector3 _mainCamPos, Vector3 _mainCamDirection, Vector3 _gunBarrelPos, Quaternion _gunBarrelRot)
+    {
+        if(Physics.Raycast(_mainCamPos, _mainCamDirection, out RaycastHit hit, Mathf.Infinity))
+        {
+            //bulletHole
+            GameObject bulletHoleInstance = Instantiate(bulletHolePrefab, hit.point + hit.normal * 0.001f, Quaternion.identity) as GameObject;
+            NetworkServer.Spawn(bulletHoleInstance);
+            bulletHoleInstance.transform.LookAt(hit.point + hit.normal);
+            Destroy(bulletHoleInstance, 2f);
+
+            //bulletTrail
+            GameObject bulletTrailInstance = Instantiate(bulletTrail, _gunBarrelPos, _gunBarrelRot);
+            NetworkServer.Spawn(bulletTrailInstance);
+            spawnBulletTrail(bulletTrailInstance, _gunBarrelPos, hit.point);
+            Destroy(bulletTrailInstance, 1f);
+
+            //check if player was hit
+            if(hit.transform.tag == "remotePlayer" || hit.transform.tag == "localPlayer")
+            {
+                Debug.Log(hit.transform.name + " has been hit");
+                ClientChangeColor(hit.transform.gameObject);
+            }
+            if(hit.transform.tag == "target")
+            {
+                ClientChangeColor(hit.transform.gameObject);
+            }
+        }
+    }
+    [ClientRpc]
+    private void spawnBulletTrail(GameObject bulletTrailInstance, Vector3 _gunBarrelPos, Vector3 hitPoint)
+    {
+        LineRenderer lineR = bulletTrailInstance.GetComponent<LineRenderer>();
+        lineR.SetPosition(0, _gunBarrelPos);
+        lineR.SetPosition(1, hitPoint);
+    }
+
+    private void weaponADS()
+    {
+        if(Input.GetKey(KeyCode.Mouse1))
+        {
+            weapon.transform.position = ADStransform.position;
+        }
+        else if(!Input.GetKey(KeyCode.Mouse1))
+        {
+            weapon.transform.position = hipFireTransform.position;
+        }
+
+    }
+    [ClientRpc]
+    private void ClientChangeColor(GameObject target)
+    {
+        if(target.tag == "target")
+        {
+            target.GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+        else
+        {
+            MeshRenderer targetMeshRenderer = target.transform.Find("playerModel").GetComponent<MeshRenderer>();
+            Color playerStartColor = targetMeshRenderer.material.color;
+            targetMeshRenderer.material.color = Color.red;
+        }
+    }
+}
